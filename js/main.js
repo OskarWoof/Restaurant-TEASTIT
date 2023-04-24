d1d770970ab863e5a1f36e6db931da180d9e4fbf
Oskar Woof <oskarwoof7@gmail.com>
2023-04-24 23:09:25 +0200

    s

--- js/main.js
+++ js/main.js
@@ -1,18 +1,27 @@
-document.querySelector(".navbar-toggler").addEventListener("click", toggleMenu);
-// console.log(toggle);
+// document.querySelector(".navbar-toggler").addEventListener("click", toggleMenu);
+// // console.log(toggle);
 
-const mainNav = document.querySelector(".mainNav");
-console.log(mainNav);
+// const mainNav = document.querySelector(".mainNav");
+// console.log(mainNav);
 
-function toggleMenu() {
-  if ((mainNav.style.display = "none")) {
-    console.log(false);
-    mainNav.classList.add("open");
-  } else {
-    // mainNav.style.dispaly = "block";
-    // mainNav.classList.remove("open");
-  }
-}
+// function toggleMenu() {
+//   if ((mainNav.style.display = "none")) {
+//     console.log(false);
+//     mainNav.classList.add("open");
+//   } else {
+//     // mainNav.style.dispaly = "block";
+//     // mainNav.classList.remove("open");
+//   }
+// }
 
 // toggleMenu();
 
+function myFunction() {
+  let myNav = document.getElementById("mainNavigation");
+  console.log(myNav);
+  if (myNav.style.display === "none") {
+    myNav.style.display = "block";
+  } else {
+    myNav.style.display = "none";
+  }
+}
